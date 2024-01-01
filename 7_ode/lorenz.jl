aea357d629584a72a091b0cdaa2711a67b811310
andremfreitas <andre.marreiros.freitas@gmail.com>
2024-01-01 23:08:09 +0000

    lorenz system plots

--- 7_ode/lorenz.jl
+++ 7_ode/lorenz.jl
@@ -26,3 +26,26 @@ tspan = (0.0,100.0)
 
 # Define our parameters
 p = (10.0,28.0,8/3)
+
+# Defining the ODE problem through DifferentialEquations.jl
+prob = ODEProblem(lorenz,u0,tspan,p)
+
+# Solve the problem by calling 'solve'
+sol = solve(prob)
+
+# Plot solution
+plot(sol)
+savefig("solution.png")
+
+# We can also plot phase space diagrams by telling it which vars to
+# compare on which axis. Let's plot this in the (x,y,z) plane:
+plot(sol,vars=(1,2,3))
+savefig("phase_space_diagram.png")
+
+# Note that the sentinal to time is 0, so we can also do (t,y,z) with:
+plot(sol,vars=(0,2,3))
+savefig("time_yz.png")
+
+println(sol(0.5))
+println(sol(0.255555))
+println(sol(0.255556))
