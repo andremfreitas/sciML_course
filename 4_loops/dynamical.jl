c95176c2fb2971048cf326f4787968a8034c14f2
andremfreitas <andre.marreiros.freitas@gmail.com>
2024-01-01 22:40:49 +0000

    Add some more content regarding dynamical systems. Mainly in the algorithmic part of reducing the loop time. Did not finish the lecture content.

--- 4_loops/dynamical.jl
+++ 4_loops/dynamical.jl
@@ -54,3 +54,70 @@ y = [to_plot[i][2] for i in 1:length(to_plot)]
 z = [to_plot[i][3] for i in 1:length(to_plot)]
 plot(x,y,z)
 savefig("lorentz.png")
+
+
+
+# Use push! instead of indexing
+function solve_system_save_push(f,u0,p,n)
+  u = Vector{typeof(u0)}(undef,1)
+  u[1] = u0
+  for i in 1:n-1
+    push!(u,f(u[i],p))
+  end
+  u
+end
+
+@time solve_system_save(lorenz,[1.0,0.0,0.0],p,1000)
+@time solve_system_save_push(lorenz,[1.0,0.0,0.0],p,1000)
+
+# Its also possible to do this comparison using benchmark tools
+using BenchmarkTools
+
+@btime solve_system_save(lorenz,[1.0,0.0,0.0],p,1000)
+@btime solve_system_save_push(lorenz,[1.0,0.0,0.0],p,1000)
+
+function solve_system_save_matrix(f,u0,p,n)
+  u = Matrix{eltype(u0)}(undef,length(u0),n)
+  u[:,1] = u0
+  for i in 1:n-1
+    u[:,i+1] = f(u[:,i],p)
+  end
+  u
+end
+
+@btime solve_system_save_matrix(lorenz,[1.0,0.0,0.0],p,1000)
+
+# Where is this cost coming from? A large portion of the cost is due 
+# to the slicing on the u, which we can fix with a view:
+
+function solve_system_save_matrix_view(f,u0,p,n)
+  u = Matrix{eltype(u0)}(undef,length(u0),n)
+  u[:,1] = u0
+  for i in 1:n-1
+    u[:,i+1] = f(@view(u[:,i]),p)
+  end
+  u
+end
+@btime solve_system_save_matrix_view(lorenz,[1.0,0.0,0.0],p,1000)
+
+
+# Since we are only ever using single columns as a unit, notice that there isn't any benefit
+# to keeping the whole thing contiguous, and in fact there are some downsides (cache is harder
+# to optimize because the longer cache lines are unnecessary, the views need to be used). Also, 
+# growing the matrix adaptively is not a very good idea since every growth requires both allocating
+# memory and copying over the old values:
+
+function solve_system_save_matrix_resize(f,u0,p,n)
+  u = Matrix{eltype(u0)}(undef,length(u0),1)
+  u[:,1] = u0
+  for i in 1:n-1
+    u = hcat(u,f(@view(u[:,i]),p))
+  end
+  u
+end
+@btime solve_system_save_matrix_resize(lorenz,[1.0,0.0,0.0],p,1000)
+
+
+"""
+This lecture continues on but it is not very useful for me atm so I'll skip it for now.
+"""
